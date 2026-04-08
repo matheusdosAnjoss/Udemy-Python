dbbff2630876eec5dd14cfca3f09fce651c9fdeb
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-04-07 21:10:45 -0300

    comentarios

--- exercicios/aula183.py
+++ exercicios/aula183.py
@@ -8,3 +8,27 @@
 # Você pode dar o nome que preferir para um
 # ambiente virtual, mas os mais comuns são:
 # venv env .venv .env
+
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+
+#
+# Ativando e desativando meu ambiente virtual
+# Windows: .\venv\Scripts\activate
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
+#
+
+# pip - instalando pacotes e bibliotecas
+# Instalar última versão:
+# pip install nome_pacote
+# Instalar versão precisa
+# (tem outras formas também não mencionadas)
+# pip install nome_pacote==0.0.0
+# Desinstalar pacote
+# pip uninstall nome_pacote
+# Congelar (ver pacotes)
+# pip freeze
