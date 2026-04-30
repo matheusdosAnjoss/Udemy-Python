9e9216a40a581629b8eb0cb3c4ee747a23234692
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-04-29 21:40:29 -0300

    Implementa leitura de JSON e reconstrução de objetos Pessoa

---
+++ POO/aula209/aula209_b.py
@@ -0,0 +1,12 @@
+import json
+from aula209_a import CAMINHO_ARQUIVO, Pessoa
+
+with open(CAMINHO_ARQUIVO, 'r') as arquivo:
+    pessoas = json.load(arquivo)
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+    p3 = Pessoa(**pessoas[2])
+
+    print(p1.nome)
+    print(p2.nome)
+    print(p3.nome)
