5f7d7828a24aaf3c9ba3d5d92d6404f142704983
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-05-01 20:12:55 -0300

    att

--- POO/aula216.py
+++ POO/aula216.py
@@ -1,20 +1,54 @@
 
 
-class Foo:
+# class Foo:
+#     def __init__(self):
+#         self.public = 'isso e publico'
+#         self._protected = 'isso é protegido'
+
+#     def metodoPublico(self):
+#         self._medoto_protegido()
+#         return 'metodo publico'
+    
+#     def _medoto_protegido(self):
+#         print('_medoto protegido')
+#         return '_medoto_protegido'
+    
+
+# f = Foo()
+
+# #print(f._protected)
+# print(f.metodoPublico())
+
+class Robo:
     def __init__(self):
-        self.public = 'isso e publico'
-        self._protected = 'isso é protegido'
+        self._pilha = 0  # Começa sem pilha
 
-    def metodoPublico(self):
-        self._medoto_protegido()
-        return 'metodo publico'
-    
-    def _medoto_protegido(self):
-        print('_medoto protegido')
-        return '_medoto_protegido'
-    
+    # O OLHINHO (Property) - para ler o valor
+    @property
+    def pilha(self):
+        print("Robô: Estou verificando quanta energia eu tenho...")
+        return f"{self._pilha}%"
 
-f = Foo()
+    # O SEGURANÇA (Setter) - para mudar o valor com cuidado
+    @pilha.setter
+    def pilha(self, valor):
+        if valor < 0:
+            print("Segurança: Ei! Você não pode tirar energia que não existe!")
+        elif valor > 100:
+            print("Segurança: Cuidado! Muita pilha vai me explodir!")
+        else:
+            print(f"Segurança: {valor}% de pilha? OK, pode colocar!")
+            self._pilha = valor
 
-#print(f._protected)
-print(f.metodoPublico())
+# --- Brincando com o Robô ---
+
+meu_robo = Robo()
+
+# Usando o SETTER para colocar 50% de pilha
+meu_robo.pilha = 50 
+
+# Usando a PROPERTY para ver quanta pilha tem
+print(meu_robo.pilha) 
+
+# Tentando fazer algo errado (o segurança vai barrar!)
+meu_robo.pilha = 999 
