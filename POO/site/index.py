01ff42ce88667d6711e420bb1d113e09f13906ef
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-06-10 22:07:54 -0300

    Att

--- POO/site/index.py
+++ POO/site/index.py
@@ -1,7 +1,7 @@
 import requests
 
 # O link (URL) do servidor que queremos consultar
-url = "https://viacep.com.br/ws/01001000/json/"
+url = "https://www.udemy.com/course/python-3-do-zero-ao-avancado/learn/lecture/35817500#content"
 
 # O HTTP entra em ação fazendo o "pedido" (GET)
 resposta = requests.get(url)
@@ -9,10 +9,11 @@ resposta = requests.get(url)
 # Verificando se o servidor respondeu com sucesso (Código 200 significa "Ok")
 if resposta.status_code == 200:
     # Transformando a resposta em um dicionário Python (JSON)
-    dados = resposta.json()
-    
-    print(f"Logradouro: {dados['logradouro']}")
-    print(f"Bairro: {dados['bairro']}")
-    print(f"Cidade: {dados['localidade']}/{dados['uf']}")
+    # dados = resposta.json()
+    print(resposta)
+    print('Funcionou!')
+    # print(f"Logradouro: {dados['logradouro']}")
+    # print(f"Bairro: {dados['bairro']}")
+    # print(f"Cidade: {dados['localidade']}/{dados['uf']}")
 else:
     print(f"Erro ao acessar o servidor: {resposta.status_code}")
