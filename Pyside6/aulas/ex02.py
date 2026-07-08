126236c5a2a82a88c868e47f6796748f32f610ae
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-08 02:50:57 +0200

    Adicionando QLayout

--- Pyside6/aulas/ex02.py
+++ Pyside6/aulas/ex02.py
@@ -1,10 +1,19 @@
 import sys
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout
 
 app = QApplication(sys.argv) # 1. Cria o aplicativo
 
 botao = QPushButton('Texto botao')# 2. Cria o botão
 botao.setStyleSheet('font-size: 50px; color: white') # 3. Estiliza (tamanho e cor)
-botao.show() # 4. Mostra o botão na tela
+# botao.show() # 4. Mostra o botão na tela
 
+central_widget = QWidget()
+
+layout = QVBoxLayout()
+central_widget.setLayout(layout)
+
+layout.addWidget(botao)
+
+
+central_widget.show()
 app.exec()# 5. Mantém a janela aberta
