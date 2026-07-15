f8c8192b7417e16b6e4aa6c7ff3f9cd6dc77c1b5
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-15 20:43:51 -0300

    Calculadora: ajustes

--- Pyside6/calculadora/main.py
+++ Pyside6/calculadora/main.py
@@ -1,6 +1,7 @@
 import sys
-
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow, QLabel
+from variaveis import WINDOW_ICON_PATH
 
 from main_window import MainWindow
 
@@ -8,10 +9,28 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
 
+    #Define o icone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
     label1 = QLabel('O meu label')
     label1.setStyleSheet('font-size: 30px')
-    window.v_layout.addWidget(label1)
+    window.addWidgetToLayout(label1)
+    
     window.ajustFixedSize()
-
     window.show()
     app.exec()
+
+# --- EXECUÇÃO DO APLICATIVO ---
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)                 # Inicializa o motor do Qt
+#     window = MainWindow()                        # Cria a janela calculadora
+
+#     label1 = QLabel('O meu label')               # Cria um texto
+#     label1.setStyleSheet('font-size: 30px')      # Aumenta a fonte do texto para 30px
+#     window.v_layout.addWidget(label1)            # Coloca o texto dentro do layout da janela
+#     window.ajustFixedSize()                      # Ajusta e trava o tamanho da janela
+
+#     window.show()                                # Exibe a janela na tela
+#     app.exec()                                   # Roda o aplicativo até ser fechado
--- Pyside6/calculadora/main_window.py
+++ Pyside6/calculadora/main_window.py
@@ -1,4 +1,5 @@
 import sys
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout, QMainWindow, QLabel
 
 class MainWindow(QMainWindow):
@@ -16,4 +17,24 @@ class MainWindow(QMainWindow):
     def ajustFixedSize(self):
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
+
+    def addWidgetToLayout(self, widget: QWidget):
+        self.v_layout.addWidget(widget)
+        
     
+
+    # --- DEFINIÇÃO DA JANELA PRINCIPAL ---
+# class MainWindow(QMainWindow):
+#     def __init__(self, /, parent: QWidget | None = None, *args, **kwargs) -> None:
+#         super().__init__(parent, *args, **kwargs) # Inicializa a classe base (QMainWindow)
+
+        # self.cw = QWidget()                      # Cria o widget central (container)
+        # self.v_layout = QVBoxLayout()            # Cria o layout vertical (alinha itens em pé)
+        # self.cw.setLayout(self.v_layout)         # Associa o layout ao container
+        # self.setCentralWidget(self.cw)           # Define o container como o centro da janela
+        
+        # self.setWindowTitle('Calculadora')        # Define o título da janela
+
+    # def ajustFixedSize(self):
+    #     self.adjustSize()                        # Ajusta a janela ao tamanho do conteúdo
+    #     self.setFixedSize(self.width(), self.height()) # Trava a janela nesse tamanho (impede redimensionar)
