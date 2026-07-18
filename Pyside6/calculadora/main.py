4da04cfcce947d339a60bb597b88d0843b66c748
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-17 21:21:41 -0300

    Tema Qdarkstyle

--- Pyside6/calculadora/main.py
+++ Pyside6/calculadora/main.py
@@ -1,37 +1,43 @@
 import sys
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow, QLabel
-from variaveis import WINDOW_ICON_PATH
+ 
+# from buttons import ButtonsGrid
 from display import Display
 from info import Info
-
 from main_window import MainWindow
-
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from style import setupTheme
+from variaveis import WINDOW_ICON_PATH
+ 
 if __name__ == '__main__':
-    #cria a aplicação
+    # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
-
-    #Define o icone
+ 
+    # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
-
-    #Info
-    info = Info('2*2 = 4')
+ 
+    # Info
+    info = Info('Sua conta')
     window.addToVLayout(info)
-
-    #Display
+ 
+    # Display
     display = Display()
     window.addToVLayout(display)
-
-    # label1 = QLabel('O meu label')
-    # label1.setStyleSheet('font-size: 30px')
-    # window.addToVLayout(label1)
-    
+ 
+    #Grid
+    # buttonsGrid = ButtonsGrid(display, info, window)
+    # window.vLayout.addLayout(buttonsGrid)
+ 
+    # Executa tudo
     window.ajustFixedSize()
     window.show()
     app.exec()
+
+
 
 # --- EXECUÇÃO DO APLICATIVO ---
 # if __name__ == '__main__':
